2c59b2a2c81648bfcf2816db941a164f8caad474
Мартин Симеонов <simeonov.martin@gmail.com>
2015-07-31 19:50:31 +0300

    Модул 09, Видео 04 и 05 - Създаване на основа за клас за експорт и добавяне на инстанция към Excel

--- Interop/Excel/IOWrite.cs
+++ Interop/Excel/IOWrite.cs
@@ -1,13 +1,51 @@
 ﻿using System;
+using InteropExcel = Microsoft.Office.Interop.Excel;
 
 namespace Excel
 {
 	public class IOWrite
 	{
 		private DataStruct _data;
+		private InteropExcel.Application excel;
 
 		public IOWrite (DataStruct data)
 		{
+		}
+
+		public bool exportTable ()
+		{
+			try
+			{
+				//Междинни проверки
+
+				return true;
+			}catch{
+			}
+
+			return false;
+		}
+
+		public void addRow ( DataRow _row )
+		{
+			try {
+
+
+
+			} catch {
+			}
+		}
+
+		public void runFile ()
+		{
+			try {
+				System.Diagnostics.Process.Start ( getPath () );
+			} catch {
+			}
+		}
+
+		private string getPath ()
+		{
+			return System.IO.Path.Combine (AppDomain.CurrentDomain.BaseDirectory, "Table1.xlsx");
 		}
 	}
 }
